cf64f80c187706396a349b569b0580dc9699dd52
mehdi <mehdi.dellegi@esprit.tn>
2023-08-11 06:49:48 -0700

    excel globale complet ca marche now

--- src/app/month-exporter-factures/month-exporter-factures.component.ts
+++ src/app/month-exporter-factures/month-exporter-factures.component.ts
@@ -4,6 +4,11 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ReleveBancaire } from '../model/ReleveBancaire';
 import * as XLSX from 'xlsx';
 import { DonneeExtrait } from '../model/DonneeExtrait';
+import Swal, { SweetAlertIcon } from 'sweetalert2';
+import { FileService } from '../service/file.service';
+import JSZip from 'jszip';
+import { saveAs } from 'file-saver';
+
 @Component({
   selector: 'app-month-exporter-factures',
   templateUrl: './month-exporter-factures.component.html',
@@ -14,6 +19,7 @@ export class MonthExporterFacturesComponent {
   months: { name: string; value: number }[] = [];
   selectedMonths: number[] = []; // Utilisation d'un tableau pour contenir les mois sélectionnés
   constructor(
+    private fileService: FileService,
     public dialogRef: MatDialogRef<MonthExporterFacturesComponent>,
     @Inject(MAT_DIALOG_DATA) public data: { releveBancaire: ReleveBancaire }
   ) {
@@ -72,6 +78,34 @@ export class MonthExporterFacturesComponent {
       this.selectedMonths
     ); // Appelle la méthode d'exportation
     this.dialogRef.close(this.selectedMonths); // Retourne les mois sélectionnés
+  }
+
+  downloadPdf(
+    url: string,
+    folderName: string
+  ): Promise<{ blob: Blob; filename: string; folderName: string }> {
+    return this.fileService
+      .downloadFile(url)
+      .toPromise()
+      .then((data) => {
+        if (!data) {
+          throw new Error('No data returned from file download');
+        }
+        const blob = new Blob([data], { type: 'application/pdf' });
+
+        let urlComponents = url.split('/');
+        // Filtrons le segment 'yt' du chemin du fichier
+        urlComponents = urlComponents.filter((segment) => segment !== 'yt');
+        // Prenez le dernier segment de l'URL modifiée, qui devrait être le nom du fichier
+        let filename = urlComponents[urlComponents.length - 1].split('?')[0];
+        filename = decodeURIComponent(filename);
+
+        // Log pour le débogage
+        console.log('Filtered URL Components:', urlComponents);
+        console.log('Final Filename:', filename);
+
+        return { blob, filename, folderName };
+      });
   }
 
   exportSelectedDataFactureToZip(
@@ -143,5 +177,80 @@ export class MonthExporterFacturesComponent {
     XLSX.writeFile(workbook, fileName);
 
     return totalRows;
+  }
+
+  async downloadInvoicesForSelectedMonths() {
+    console.log('methodes factureeessss!!!!!');
+    // Filtrer les extraits pour les mois sélectionnés par l'utilisateur
+    const selectedExtraits = this.data.releveBancaire.extraits.filter(
+      (extrait) => {
+        const date = new Date(extrait.dateExtrait);
+        return this.selectedMonths.includes(date.getMonth() + 1);
+      }
+    );
+
+    let downloadPromises: Promise<{
+      blob: Blob;
+      filename: string;
+      folderName: string;
+    }>[] = [];
+
+    selectedExtraits.forEach((extrait) => {
+      extrait.donneeExtraits.forEach((donnee) => {
+        if (Object.keys(donnee.associationTitreUrl).length > 0) {
+          for (let url of Object.values(donnee.associationTitreUrl)) {
+            if (url) {
+              let folderName = this.formatDate(new Date(extrait.dateExtrait));
+              let downloadPromise = this.downloadPdf(url, folderName);
+              downloadPromises.push(downloadPromise);
+            }
+          }
+        }
+      });
+    });
+
+    // Regrouper les factures téléchargées dans un fichier ZIP pour le téléchargement
+    Promise.all(downloadPromises)
+      .then(async (fileObjects) => {
+        let title = 'Information';
+        let text = '';
+        let icon: SweetAlertIcon = 'info';
+
+        if (fileObjects.length === 0) {
+          text = `Aucune facture n'a été trouvée pour les mois sélectionnés.`;
+          icon = 'error';
+        } else {
+          let zip = new JSZip();
+
+          fileObjects.forEach(({ blob, filename, folderName }) => {
+            zip.file(folderName + '/' + filename, blob);
+          });
+
+          const timestamp = new Date().toISOString();
+          await zip.generateAsync({ type: 'blob' }).then((content) => {
+            saveAs(content, `factures_${timestamp}.zip`);
+          });
+
+          text = `Les factures ont été téléchargées avec succès. Vérifiez vos téléchargements.`;
+          icon = 'success';
+        }
+
+        await Swal.fire({
+          title: title,
+          text: text,
+          icon: icon,
+          confirmButtonColor: '#3085d6',
+          confirmButtonText: 'OK',
+        });
+
+        // fermer la boîte de dialogue après le téléchargement
+        this.dialogRef.close();
+      })
+      .catch((error) => {
+        console.error(
+          'Une erreur est survenue lors du téléchargement des fichiers:',
+          error
+        );
+      });
   }
 }
